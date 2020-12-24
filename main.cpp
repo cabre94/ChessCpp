19ccdb9f63760c457cc2c4beedbfd367b2137943
Cabre <cabre94@hotmail.com>
2020-12-24 13:31:34 -0300

    Minor changes

--- main.cpp
+++ main.cpp
@@ -8,55 +8,72 @@ GitLab: https://gitlab.com/cabre94
 Description:
 */
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <list>
-#include <fstream>
-#include <cmath>
-#include <cstdlib>
-#include <algorithm>
-#include <sstream>
 
-#include "Board.cpp"
+#include "ChessGame.hpp"
 // #include "Piece.cpp"
+// #include "Board.hpp"
+// #include "ChessBoard.hpp"
+#include <cstdlib>
+
+// #include "Board.cpp"
+// #include "Piece.cpp"
+// #include "ChessBoard.cpp"
+
+// int main(int argc, const char** argv){
+    
+//     Board* ptr;
+//     // ChessBoard* ptr;
+
+//     ptr = new ChessBoard;
+
+//     // system("clear");
+//     // ptr->printBoard();
+//     // ptr->printPositions();
+
+//     int counter = 0;
+
+//     while(!ptr->askWinner()){
+//         // system("clear");
+
+//         bool succesMove;
+
+//         std::string from, to;
+
+//         ptr->printPosAndPieces();
+//         ptr->printBoard();
+
+//         // std::cout << "From: ";
+//         // std::cin >> from;
+//         // std::cout << "To: ";
+//         // std::cin >> to;
+
+//         succesMove = ptr->makeMove(from, to);
+
+//         if(succesMove)
+//             counter++;
+
+//     }
+
+//     // ptr->printBoard();
+//     // ptr->printPositions();
+    
+//     delete ptr;
+
+//     // ptr = new T2;
+//     // ptr->print();
+
+//     return 0;
+// }
+
+
+
+
 
 int main(int argc, const char** argv){
     
-    Board* ptr;
+    ChessGame game;
 
-    ptr = new ChessBoard;
-    ptr->printBoard();
-    ptr->printPositions();
-
-    int counter = 0;
-
-    while(counter < 5){
-        bool succesMove;
-
-        std::string from, to;
-
-        std::cout << "From: ";
-        std::cin >> from;
-        std::cout << "To: ";
-        std::cin >> to;
-
-        succesMove = ptr->makeMove(from, to);
-
-        ptr->printPositions();
-        ptr->printBoard();
-
-        if(succesMove)
-            counter++;
-    }
-
-    // ptr->printBoard();
-    // ptr->printPositions();
-    
-    delete ptr;
-
-    // ptr = new T2;
-    // ptr->print();
+    game.play();
 
     return 0;
 }
