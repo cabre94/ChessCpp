7123e22b21597aca72e11dd6907a253ea4bca3bb
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-06 17:12:28 -0300

    add positions tests #13

--- test/test_Positions.cpp
+++ test/test_Positions.cpp
@@ -13,13 +13,49 @@
 // #include "Queen.h"
 // #include "Rook.h"
 
+static std::string toLower(const std::string &str) {
+    std::string lowerStr = str; // Copiar el string original
+    std::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+    return lowerStr;
+}
+
 TEST(chessCpp, test_Positions) {
-    static const uint32_t PLAYER_ID = 0xB0CA;
-    static const chess::Position expected_pos = "A1";
 
-    const chess::Bishop bishop(PLAYER_ID, expected_pos);
+    char c[] = "A1";
+    std::string str = "A1";
+    std::string str_lower;
 
-    ASSERT_EQ(bishop.getPlayerID(), PLAYER_ID);
-    ASSERT_EQ(bishop.getName(), "BISHOP");
-    ASSERT_EQ(bishop.getPosition(), expected_pos);
+    for (char row = '1'; row <= '8'; row++) {
+        for (char col = 'A'; col <= 'H'; col++) {
+
+            // Update expected values
+            c[0] = col;
+            c[1] = row;
+            str[0] = col;
+            str[1] = row;
+            str_lower = toLower(str);
+
+            // Create Position with std::string
+            const chess::Position pos_str(str);
+
+            // Check indexes
+            ASSERT_EQ(pos_str[0], col - 'A');
+            ASSERT_EQ(pos_str[1], row - '1');
+            ASSERT_EQ(pos_str.str(), str_lower);
+
+            // Create Position with const char*
+            const chess::Position pos_c(c);
+
+            // Check indexes
+            ASSERT_EQ(pos_c[0], col - 'A');
+            ASSERT_EQ(pos_c[1], row - '1');
+            ASSERT_EQ(pos_c.str(), str_lower);
+
+            ASSERT_TRUE(pos_str == pos_c);
+            ASSERT_FALSE(pos_str != pos_c);
+            ASSERT_FALSE(pos_str < pos_c);
+            ASSERT_FALSE(pos_c < pos_str);
+        }
+    }
 }
