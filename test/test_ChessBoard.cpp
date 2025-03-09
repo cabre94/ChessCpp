681015248ab0dfd5dd31de306b45de15beecd328
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-08 21:44:28 -0300

    getParallelMoves test #20

--- test/test_ChessBoard.cpp
+++ test/test_ChessBoard.cpp
@@ -23,28 +23,52 @@
 //     return lowerStr;
 // }
 
-static void printSetPositions(const std::set<chess::Position> &positions) {
+[[maybe_unused]] static void printSetPositions(const chess::Position &c_pos,
+                                               const std::set<chess::Position> &positions) {
+    std::cout << c_pos << " --> ";
     for (const auto &pos : positions) {
         std::cout << pos << " "; // Usa el operador << definido en Position
     }
     std::cout << std::endl;
 }
 
+static std::set<chess::Position> expectedParallelMovesEmptyBoard(uint32_t r, uint32_t c) {
+    std::set<chess::Position> exp_set;
+
+    // Add positions in the same row
+    for (uint32_t cc = 0; cc < chess::ChessBoard::N_COL; cc++) {
+        if (cc != c)
+            exp_set.insert(chess::Position(r, cc));
+    }
+
+    // Add positions in the same column
+    for (uint32_t rr = 0; rr < chess::ChessBoard::N_ROW; rr++) {
+        if (rr != r)
+            exp_set.insert(chess::Position(rr, c));
+    }
+
+    return exp_set;
+}
+
 TEST(ChessBoard, getParallelMoves) {
     chess::ChessBoard board;
 
-    std::set<chess::Position> moves;
+    std::set<chess::Position> moves, expected_moves;
     chess::Position pos(0, 0);
 
     for (uint32_t r = 0; r < chess::ChessBoard::N_ROW; r++) {
         for (uint32_t c = 0; c < chess::ChessBoard::N_COL; c++) {
+            // Get expected set of valid moves for this position
+            expected_moves = expectedParallelMovesEmptyBoard(r, c);
+
             // Create current position
             pos = chess::Position(r, c);
 
+            // Get moves from board
             moves = board.getParallelMoves(pos, chess::WHITE);
-            // moves.size()
 
-            printSetPositions(moves);
+            printSetPositions(pos, moves);
+            EXPECT_EQ(moves, expected_moves) << "Error en posición (" << r << ", " << c << ")";
         }
     }
 }
