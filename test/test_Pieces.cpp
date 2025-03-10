65b719ac24a6cab1e6fc7d78dcf783fec625c801
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-10 12:40:07 -0300

    Upodate Pieces tests #29

--- test/test_Pieces.cpp
+++ test/test_Pieces.cpp
@@ -4,22 +4,54 @@
 #include <string>
 
 #include "Pieces/Bishop.h"
+#include "Pieces/Champion.h"
+#include "Pieces/King.h"
+#include "Pieces/Knight.h"
+#include "Pieces/Magician.h"
+#include "Pieces/Pawn.h"
+#include "Pieces/Queen.h"
+#include "Pieces/Rook.h"
 #include "Positions/Position.h"
-// #include "Champion.h"
-// #include "King.h"
-// #include "Knight.h"
-// #include "Magician.h"
-// #include "Pawn.h"
-// #include "Queen.h"
-// #include "Rook.h"
+
+static chess::Position getRandomValidPosition() {
+    uint32_t r = rand() % 8;
+    uint32_t c = rand() % 8;
+
+    return chess::Position(r, c);
+}
 
 TEST(Pieces, Getters) {
-    static const chess::PlayerID player_id = chess::WHITE;
-    static const chess::Position expected_pos = "A1";
+    srand((unsigned int) time(0));
 
-    const chess::Bishop bishop(player_id, expected_pos);
+    std::vector<chess::PlayerID> exp_player_id;
+    std::vector<chess::Position> exp_pos;
+    std::string exp_names[] = {chess::BISHOP_NAME, chess::CHAMPION_NAME, chess::KING_NAME,
+                               chess::KNIGHT_NAME, chess::MAGICIAN_NAME, chess::PAWN_NAME,
+                               chess::QUEEN_NAME,  chess::ROOK_NAME};
 
-    ASSERT_EQ(bishop.getPlayerID(), player_id);
-    ASSERT_EQ(bishop.getName(), "BISHOP");
-    ASSERT_EQ(bishop.getPosition(), expected_pos);
+    // Generate random expected variables
+    for (size_t i = 0; i < 8; ++i) {
+        exp_player_id.push_back((chess::PlayerID)(rand() & 1));
+        exp_pos.push_back(getRandomValidPosition());
+    }
+
+    // Generate pieces with random params
+    chess::Piece *piece[] = {new chess::Bishop(exp_player_id[0], exp_pos[0]),
+                             new chess::Champion(exp_player_id[1], exp_pos[1]),
+                             new chess::King(exp_player_id[2], exp_pos[2]),
+                             new chess::Knight(exp_player_id[3], exp_pos[3]),
+                             new chess::Magician(exp_player_id[4], exp_pos[4]),
+                             new chess::Pawn(exp_player_id[5], exp_pos[5]),
+                             new chess::Queen(exp_player_id[6], exp_pos[6]),
+                             new chess::Rook(exp_player_id[7], exp_pos[7])};
+
+    for (size_t i = 0; i < 8; ++i) {
+        ASSERT_EQ(piece[i]->getPlayerID(), exp_player_id[i]);
+        ASSERT_EQ(piece[i]->getName(), exp_names[i]);
+        ASSERT_EQ(piece[i]->getPosition(), exp_pos[i]);
+    }
+
+    // Free memory
+    for (size_t i = 0; i < 8; ++i)
+        delete piece[i];
 }
